0e7c2cd9fa13d055d6ba583361080c3ee68cf4ca
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-03-26 15:03:41 +0600

    support for mongolian

--- get-mapping-for-language.rb
+++ get-mapping-for-language.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 require "./russian-ipa-mapping"
+require "./mongol-ipa-mapping"
 
 def get_mapping_for_language language
 	return russian_ipa_mapping if language == "russian"
+	return mongol_ipa_mapping if language == "mongolian"
 end
